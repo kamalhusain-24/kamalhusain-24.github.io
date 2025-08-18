13d6f204c66bc3b26a60882a610dd6f424114f7d
Kamal Husain <kamalhusain.net@gmail.com>
2025-08-18 10:35:58 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,1 @@
-// This file contains the application's configuration and sensitive keys.
-// Keep this file secure and do not share it publicly.
 
-// We are attaching the config to the global 'window' object
-// so it can be accessed by other scripts like app.js
-window.appConfig = {
-    firebaseConfig: {
-        apiKey: "AIzaSyB4iZR0E0kYgq4XsWHRGSrIo_35E62LbNM",
-        authDomain: "gemini-text-to-speech-ai.firebaseapp.com",
-        databaseURL: "https://gemini-text-to-speech-ai-default-rtdb.firebaseio.com",
-        projectId: "gemini-text-to-speech-ai",
-        storageBucket: "gemini-text-to-speech-ai.appspot.com",
-        messagingSenderId: "950716166956",
-        appId: "1:950716166956:web:10c1d9f9a47e5311443289",
-        measurementId: "G-XE5F8LQHXG"
-    },
-    DEFAULT_GEMINI_API_KEY: "AIzaSyAIlEZV4mGFgP8SxhR_dApr5RLY4H7nPEk",
-    ADMIN_UID: 'KpBdVxFTuiUVt6FZtu9zLZpLBVt2'
-};
